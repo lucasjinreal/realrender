6f03e77a0a7095e17a0980e1ffc6648732c1046f
Your Name <you@example.com>
2022-02-12 21:04:57 +0800

    add render config for human mesh

--- realrender/render.py
+++ realrender/render.py
@@ -37,8 +37,53 @@ def render(img, ver_lst, tri, alpha=0.6, show_flag=False, wfp=None, with_bg_flag
         overlap = np.zeros_like(img)
 
     for ver_ in ver_lst:
-        ver = _to_ctype(ver_.T)  # transpose
+        ver = _to_ctype(ver_)  # transpose
         overlap = render_app(ver, tri, overlap)
+
+    if with_bg_flag:
+        res = cv2.addWeighted(img, 1 - alpha, overlap, alpha, 0)
+    else:
+        res = overlap
+
+    if wfp is not None:
+        cv2.imwrite(wfp, res)
+        print(f'Save visualization result to {wfp}')
+
+    return res
+
+
+'''
+predefined config to render human mesh
+'''
+
+cfg2 = {
+    'intensity_ambient': 0.3,
+    'color_ambient': (1, 1, 1),
+    'intensity_directional': 0.6,
+    'color_directional': (1, 1, 1),
+    'intensity_specular': 0.1,
+    'specular_exp': 5,
+    'light_pos': (0, 0, -5),
+    'view_pos': (0, 0, 5)
+}
+
+render_app2 = RenderPipeline(**cfg2)
+
+
+def render_human_mesh(img, ver_lst, tri, alpha=0.6, show_flag=False, wfp=None, with_bg_flag=True):
+    '''
+    ver: [(3, 38365)]
+    tri: (76073, 3)
+    '''
+    # print(ver_lst)
+    if with_bg_flag:
+        overlap = img.copy()
+    else:
+        overlap = np.zeros_like(img)
+
+    for ver_ in ver_lst:
+        ver = _to_ctype(ver_)  # transpose
+        overlap = render_app2(ver, tri, overlap)
 
     if with_bg_flag:
         res = cv2.addWeighted(img, 1 - alpha, overlap, alpha, 0)
